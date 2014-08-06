3edcae472943c3ac05a3d208bc414625214691ee
NurErkizilkaya <nur.er900@gmail.com>
2014-08-06 11:16:58 +0300

    Deneme added.

---
+++ src/com/pia/test/ucgen.java
@@ -0,0 +1,13 @@
+package com.pia.test;
+
+public class ucgen {
+
+	public static void main(String[] args) {
+	for(int i=1;i<=5;i++){
+		for(int k=0;k<i;k++){
+			System.out.print("*");	
+		}
+		System.out.println("");
+	}
+	}
+}
