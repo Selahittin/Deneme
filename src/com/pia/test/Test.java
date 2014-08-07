27e31a2cd8d444626a82bac3038054929b2a24cd
NurErkizilkaya <nur.er900@gmail.com>
2014-08-07 13:49:35 +0300

    Deneme finished.

--- src/com/pia/test/Test.java
+++ src/com/pia/test/Test.java
@@ -1,17 +1,32 @@
 package com.pia.test;
 
+import java.util.Scanner;
+
+
 public class Test {
 
 	public static void main(String[] args) {
-	for(int i=50;i>=0;i--){
-		System.out.print("  ");
-		System.out.print("*");
+int sayi=0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("cap belirle:");
+	     sayi = input.nextInt();
+	    
+	    for(int j=0;j<sayi;j++){
+	    	for(int k=1;k<sayi;k++){
+	    		System.out.print("* ");
+	    	}
+          System.out.print("* ");
+          System.out.println();
+	         }
 		
 		
-		
-		
+	    
+	    
 	}
+				
+			}
+		
+		
 
-	}
 
-}
+	
