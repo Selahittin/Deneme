d0c32e343e5eae43deea841c37e2c3bfbbe9e815
toshiba <toshiba@toshibaa>
2014-08-20 12:16:22 +0300

    Deneme class example.

---
+++ Deneme/src/piabilisim/staj/example1.java
@@ -0,0 +1,10 @@
+package piabilisim.staj;
+import javax.swing.JOptionPane;
+public class example1 {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog( null, "Hello...Welcome to my java world..." );
+
+	}
+
+}
