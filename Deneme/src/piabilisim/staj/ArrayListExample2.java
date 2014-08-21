c49536fa9c790eb88478511001417a209e0d01f2
toshiba <toshiba@toshibaa>
2014-08-21 14:41:38 +0300

    ArrayList example program.

---
+++ Deneme/src/piabilisim/staj/ArrayListExample2.java
@@ -0,0 +1,17 @@
+package piabilisim.staj;
+
+import java.util.ArrayList;
+
+public class ArrayListExample2 {
+
+	public static void main(String[] args) {
+		ArrayList<String> arrayList= new ArrayList<String>();
+		arrayList.add("\n"+"****"
+				    + "\t" +"****"
+				    + "\t"+"****"
+				    + "\t"+"****"
+				    + "\t"+"****");
+		System.out.println("ArrayList in Icerigi: " + arrayList);
+	}
+
+}
